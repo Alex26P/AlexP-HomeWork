c485f6e5aaed79677ee020de2eade74ebd18ac98
Alex26P <lavinia.alexandra_p@yahoo.com>
2018-11-26 17:31:07 +0200

    longest seq

---
+++ src/s05/LongestSequence.java
@@ -0,0 +1,34 @@
+package s05;
+
+public class LongestSequence {
+
+	public static void main(String[] args) {
+
+		int numbers[] = { 1, 1, 3, 3, 6, 4, 7, 7, 7, 9, 9, 9, 9, 9, 3, 6 };
+
+		System.out.println(getSequence(numbers));
+	}
+
+	private static int getSequence(int[] numbers) {
+
+		int counter = 0;
+		int counter_max = 0;
+		int y;
+		y = numbers[0];
+		for (int x = 1; x < numbers.length; x++) {
+			if (numbers[x] == y) {
+				counter++;
+
+			} else {
+				counter = 1;
+			}
+			if (counter_max < counter) {
+				counter_max = counter;
+			}
+			y = numbers[x];
+
+		}
+		return counter_max;
+	}
+
+}
