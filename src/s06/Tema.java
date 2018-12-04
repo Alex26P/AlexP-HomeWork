80e4d45bbe547fdda25144f931f4e629f7699349
Alex26P <lavinia.alexandra_p@yahoo.com>
2018-12-04 20:29:10 +0200

    ff

--- src/s06/Tema.java
+++ src/s06/Tema.java
@@ -1,5 +1,21 @@
 package s06;
 
 public class Tema {
+	
+public static void main(String[] args) {
+	
+	Tema division = new Tema();
+	division.getSomething(3,5);
+	
+}
 
+	private int getSomething (int a, int b) {
+		if (b==0) {
+			return -1;
+			
+		}else {
+			
+	return a/b;
+	}
+}
 }
